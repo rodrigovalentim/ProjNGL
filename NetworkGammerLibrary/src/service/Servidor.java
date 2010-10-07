aabbb33e1db382c86909fae8108152d4661ecb25
Rodrigo Valentim <rodrigo.valentim@gmail.com>
2010-10-07 00:10:06 -0300

    first commit

---
+++ NetworkGammerLibrary/src/service/Servidor.java
@@ -0,0 +1,29 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package service;
+
+import contratos.GammerInterface;
+import javax.swing.JDesktopPane;
+
+/**
+ *
+ * @author Rodrigo
+ */
+public class Servidor extends Thread implements GammerInterface {
+
+    public void setModoServidor() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void inicializar(JDesktopPane desktopPane, int porta, String ip) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void comunicacao() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+}
